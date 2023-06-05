f0a59169c6220f51d49a6ff11cf5b78061e2be8f
hermannKonyar <er.konyar@gmail.com>
2023-06-05 17:56:01 +0300

    First Commit

---
+++ ApiConsume/HotelProject.EntityLayer/Concrete/Testimonial.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelProject.EntityLayer.Concrete
+{
+    public class Testimonial
+    {
+        public int TestimonialID { get; set; }
+        public int Name { get; set; }
+        public int Title { get; set; }
+        public int Description { get; set; }
+        public int Image { get; set; }
+    }
+}
