20ea226449e1818693d946dae45fa3d8fce78eed
hermannKonyar <er.konyar@gmail.com>
2023-06-09 18:01:16 +0300

    Third Commit

---
+++ ApiConsume/HotelProject.WebApi/Controllers/StaffController.cs
@@ -0,0 +1,50 @@
+﻿using HotelProject.BusinessLayer.Abstract;
+using HotelProject.EntityLayer.Concrete;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HotelProject.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StaffController : ControllerBase
+    {
+        private readonly IStaffService _StaffService;
+
+        public StaffController(IStaffService StaffService)
+        {
+            _StaffService = StaffService;
+        }
+
+        [HttpGet]
+        public IActionResult StaffList()
+        {
+            var values = _StaffService.TGetList();
+            return Ok(values);
+        }
+        [HttpPost]
+        public IActionResult AddStaff(Staff Staff)
+        {
+            _StaffService.TInsert(Staff);
+            return Ok();
+        }
+
+        [HttpDelete]
+        public IActionResult DeleteStaff()
+        {
+            return Ok();
+        }
+
+        [HttpPut]
+        public IActionResult UpdateStaff()
+        {
+            return Ok();
+        }
+
+        [HttpPost("{id}")]
+        public IActionResult GetStaff()
+        {
+            return Ok();
+        }
+    }
+}
---
+++ ApiConsume/HotelProject.WebApi/Program.cs
@@ -0,0 +1,35 @@
+using HotelProject.BusinessLayer.Abstract;
+using HotelProject.BusinessLayer.Concrete;
+using HotelProject.DataAccessLayer.Abstract;
+using HotelProject.DataAccessLayer.Concrete;
+using HotelProject.DataAccessLayer.EntityFramework;
+using HotelProject.DataAccessLayer.Repositories;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<Context>();
+// Add services to the container.
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddScoped<IStaffDal, EfStaffDal>();
+builder.Services.AddScoped<IStaffService, StaffManager>();
+builder.Services.AddScoped<IRoomDal, EfRoomDal>();
+builder.Services.AddScoped<IRoomService, RoomManager>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
