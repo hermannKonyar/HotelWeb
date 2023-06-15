2c72aba4d8db6a5ee25057516570be7633e939f3
hermannKonyar <er.konyar@gmail.com>
2023-06-15 20:52:58 +0300

    Seventh Commit

--- Frontend/HotelProject.WebUI/Controllers/AdminLayoutController.cs
+++ Frontend/HotelProject.WebUI/Controllers/AdminLayoutController.cs
@@ -8,5 +8,37 @@ namespace HotelProject.WebUI.Controllers
         {
             return View();
         }
+
+        public PartialViewResult HeadPartial()
+        {
+            return PartialView();
+        }
+        
+        public PartialViewResult PreloaderPartial()
+        {
+            return PartialView();
+        }
+        
+        public PartialViewResult NavHeaderPartial()
+        {
+            return PartialView();
+        }
+        public PartialViewResult HeaderPartial()
+        {
+            return PartialView();
+        }
+        public PartialViewResult SidebarPartial()
+        {
+            return PartialView();
+        }
+        public PartialViewResult FooterPartial()
+        {
+            return PartialView();
+        }
+        public PartialViewResult ScriptPartial()
+        {
+            return PartialView();
+        }
+
     }
 }
